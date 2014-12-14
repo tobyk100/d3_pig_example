622e58e93c22351c8f403356952dde32a9bd66c3
Toby Kahan <tobyk100@gmail.com>
2014-12-14 20:22:24 +0200

    Add graphing for conversion rate and failure rate.

--- analyze.R
+++ analyze.R
@@ -1,29 +1,71 @@
 events <- read.csv(file='events.csv', header=TRUE)
 premium_users <- read.csv(file='premium.csv', header=TRUE)
-
 premium_users <- premium_users$user.id
 
-use <- events$event == 'provision Blog'
-blog_users <- unique(events[use, 2])
-rm(use)
+filter_events_by_type <- function(type) {
+  use <- events$event == type
+  users <- unique(events[use, 2])
+  rm(use)
+  return(users)
+}
 
-use <- events$event == 'provision eComm'
-ecomm_users <- unique(events[use, 2])
-rm(use)
+users_with_failure = filter_events_by_type('save Failure')
+
+user_stats <- function(users) {
+  premium_users = intersect(users, premium_users)
+  conversion_rate = 100.0 * length(premium_users) / length(users)
+  blog_only_users_with_failure = intersect(users_with_failure, blog_only_users)
+  failed = intersect(users, users_with_failure)
+  failure_rate = 100.0 * length(failed) / length(users)
+  stats = list(users = users,
+               premium_users = premium_users,
+               conversion_rate = conversion_rate,
+               failed = failed,
+               failure_rate = failure_rate)
+  return(stats)
+}
+
+blog_users = filter_events_by_type('provision Blog')
+ecomm_users = filter_events_by_type('provision eComm')
 
 blog_only_users <- setdiff(blog_users, ecomm_users)  # R setdiff is assymetric
 ecomm_only_users <- setdiff(ecomm_users, blog_users)  # R setdiff is assymetric
 blog_and_ecomm_users <- intersect(blog_users, ecomm_users)
 vanilla_users <- setdiff(events$user.id, union(blog_users, ecomm_users))
 
-blog_premium_users <- intersect(blog_users, premium_users)
-ecomm_premium_users <- intersect(ecomm_users, premium_users)
-blog_and_ecomm_premium_users <- intersect(blog_and_ecomm_users, premium_users)
-vanilla_premium_users <- intersect(vanilla_users, premium_users)
+blog_only_stats = user_stats(blog_only_users)
+ecomm_only_stats = user_stats(ecomm_only_users)
+blog_and_ecomm_stats = user_stats(blog_and_ecomm_users)
+vanilla_stats = user_stats(vanilla_users)
 
-blog_only_conversion_rate = length(blog_premium_users) / length(blog_only_users)
-ecomm_only_conversion_rate = length(ecomm_premium_users) / length(ecomm_only_users)
-blog_and_ecomm_conversion_rate = length(blog_and_ecomm_premium_users) / length(blog_and_ecomm_users)
-vanilla_conversion_rate = length(vanilla_premium_users) / length(vanilla_users)
+# Conversion Rates
+#TODO(toby): top bar goes off scale, add colors, increase label font size
+graph_barplot_of_field <- function(field) {
+}
 
-barplot(c(blog_only_conversion_rate, ecomm_only_conversion_rate))
+barplot(t(c(blog_only_stats$conversion_rate,
+            ecomm_only_stats$conversion_rate,
+            blog_and_ecomm_stats$conversion_rate,
+            vanilla_stats$conversion_rate)),
+        names = c("Blog Only",
+                  "Ecomm Only",
+                  "Blog and Ecomm",
+                  "Vanilla"),
+        main = "Conversion Rate by User Type",
+        ylab = "Conversion Rate %",
+        xlab = "User Type")
+dev.new()
+premium_user_failure_rate = length(intersect(users_with_failure, premium_users)) / length(premium_users)  # 0.0
+barplot(t(c(blog_only_stats$failure_rate,
+            ecomm_only_stats$failure_rate,
+            blog_and_ecomm_stats$failure_rate,
+            vanilla_stats$failure_rate,
+            premium_user_failure_rate)),
+        names = c("Blog Only",
+                  "Ecomm Only",
+                  "Blog and Ecomm",
+                  "Vanilla",
+                  "Premium"),
+        main = "Failure Rate by User Type",
+        ylab = "Failure Rate %",
+        xlab = "User Type")
