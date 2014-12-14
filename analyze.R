c1652e78e5cb5bfbb2db2c3c3f0e9087c03959e1
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2014-12-15 00:51:56 +0200

    Code cleanup and proportion plotting
    Cleaned up and refactored messy or redundant code. Added functions to
    plot trend and proportion of failures alongside each other for any time
    interval using either `show_trend_by_minutes` or `show_trend_by_hours`

--- analyze.R
+++ analyze.R
@@ -27,9 +27,14 @@ blog_premium_users <- intersect(blog_users, premium_users)
 ecomm_premium_users <- intersect(ecomm_users, premium_users)
 blog_and_ecomm_premium_users <- intersect(blog_and_ecomm_users, premium_users)
 
+timestamp_format <- "%Y-%d-%mT%H:%M:%S"
+#Get the timestamps for save start events
+use <- events$event == 'save start'
+start_times <- strptime(events[use, 1], format=timestamp_format)
+
 #Get the timestamps for failures. Currently missing sub-second values
 use <- events$event == 'save Failure'
-failure_times <- strptime(events[use, 1], format="%Y-%d-%mT%H:%M:%S")
+failure_times <- strptime(events[use, 1], format=timestamp_format)
 bins_by_hour <- cut(failure_times,
                     breaks=seq(from=min(failure_times),
                                to=max(failure_times),
@@ -39,20 +44,34 @@ bins_by_quarter_hour <- cut(failure_times,
                             breaks=seq(from=min(failure_times),
                                to=max(failure_times),
                                by=as.difftime(c("15"), format="%M")))
+
+#define the layout properties for the plots
+two_plot_layout <- function(){
+    layout(matrix(data=c(1,2), nrow=1, ncol=2))
+}
+
 show_trend_by_hours <- function(h=1){
     tdiff <- as.difftime(c(h), units="hours")
-    bins <- cut(failure_times,
-                breaks=seq(from=min(failure_times),
-                           to=max(failure_times),
-                           by=tdiff))
-    barplot(table(bins))
+    show_trend(tdiff)
+    
 }
 
 show_trend_by_minutes <- function(m=15){
     tdiff <- as.difftime(c(m), units="mins")
-    bins <- cut(failure_times,
-                breaks=seq(from=min(failure_times),
-                           to=max(failure_times),
-                           by=tdiff))
-    barplot(table(bins))
+    show_trend(tdiff)
 }
+
+show_trend <- function(tdiff){
+    interval_breaks <- seq(from=min(start_times),
+                           to=max(start_times),
+                           by=tdiff)
+    fbins <- cut(failure_times, breaks=interval_breaks)
+    total_bins <- cut(start_times, breaks=interval_breaks)
+    two_plot_layout()
+    barplot(table(fbins),
+            main=paste(c("Number of failures every", paste(tdiff, units(tdiff)))))
+    barplot(table(fbins) / table(total_bins),
+            main=paste(c("Proportion of failures every", paste(tdiff, units(tdiff)))))
+}
+
+
