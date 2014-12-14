fa79f6d6197c0db5233978670714570747bd0a7b
Toby Kahan <tobyk100@gmail.com>
2014-12-14 18:06:41 +0200

    Fix vanilla users calculation. Add blog and ecomm only user sets.

--- analyze.R
+++ analyze.R
@@ -1,5 +1,6 @@
 events <- read.csv(file='events.csv', header=TRUE)
 premium_users <- read.csv(file='premium.csv', header=TRUE)
+
 premium_users <- premium_users$user.id
 num_unique_users <- length(unique(events$user.id))
 
@@ -16,6 +17,12 @@ rm(use)
 blog_and_ecomm_users <- intersect(blog_users, ecomm_users)
 num_BandE_users <- length(intersect(blog_users, ecomm_users))
 
-vanilla_users <- setdiff(events$user.id, blog_and_ecomm_users)
+blog_only_users <- setdiff(blog_users, ecomm_users)  # R setdiff is assymetric
+ecomm_only_users <- setdiff(ecomm_users, blog_users)  # R setdiff is assymetric
+
+vanilla_users <- setdiff(events$user.id, union(blog_users, ecomm_users))
 
 vanilla_premium_users <- intersect(vanilla_users, premium_users)
+blog_premium_users <- intersect(blog_users, premium_users)
+ecomm_premium_users <- intersect(ecomm_users, premium_users)
+blog_and_ecomm_premium_users <- intersect(blog_and_ecomm_users, premium_users)
